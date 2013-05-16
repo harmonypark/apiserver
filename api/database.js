ba76470cb9e52c4f7d0458ca5b669fbf1e4cf98b
eleventigers <phonodelik@gmail.com>
2013-05-16 16:53:29 +0100

    db cache set to false

--- api/database.js
+++ api/database.js
@@ -6,6 +6,8 @@ var db = require(__dirname + "/mongo-wrapper.js");
 db.poolEnabled = true;
 db.poolSize = 20;
 db.killEnabled = true;
+db.cacheEnabled = false;
+db.defaultCacheTime = 60;
 db.setDatabases(config.mongo);
 db.playtomic.collections(["games", "gamevars", "leaderboard_scores", "leaderboard_tables", "leaderboard_bans", "playerlevel_levels", "playerlevel_bans"]);
 
